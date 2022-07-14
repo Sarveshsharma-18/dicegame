2422b2f4f2fe234d47ab359f884131cb58bb6b11
sarvesh <sarveshsharmaji1@gmail.com>
2022-07-14 20:47:29 +0530

    diceAppCompleated

---
+++ lib/page1.dart
@@ -0,0 +1,18 @@
+// import 'package:dicegame/routes.dart';
+// import 'package:flutter/material.dart';
+
+// class FirstScreen extends StatelessWidget {
+//   const FirstScreen({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.tealAccent,
+//       appBar: AppBar(
+//         backgroundColor: Colors.teal,
+//       ),
+//       body: ElevatedButton(onPressed: Navigator.push(context, MyRoutes.secondScreen), child: child),
+//     );
+
+//   }
+// }
---
+++ lib/routes.dart
@@ -0,0 +1,7 @@
+import 'package:dicegame/page1.dart';
+
+class MyRoutes {
+  static String homeRoute = "/home";
+  static String firstScreen = "/first";
+  static String secondScreen = "/second";
+}
